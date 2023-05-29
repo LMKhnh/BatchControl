dcd27507b74a6e960d3d92dfaf91f2e6f85bf6d7
LMKhnh <lemykhanh20092002@gmail.com>
2023-05-29 11:24:57 +0700

    up HTPhaMau

---
+++ Source/Simit/mem/components.mli
@@ -0,0 +1,496 @@
+﻿#VERSION 100
+COMPONENT {
+NAME=s_PTnode#2
+INFO {
+GUID=g_f_000hsn_51454n9n
+CYCLE=i_21
+ACTIVATED=True
+OCCURER=s_proj\Chart.diag
+}
+}
+COMPONENT {
+NAME=s_DriveV1#6
+INFO {
+GUID=g_h_4dvxqbg4s95_007n4ybjh1yrv
+CYCLE=i_21
+ACTIVATED=True
+OCCURER=s_proj\Chart.diag
+}
+}
+COMPONENT {
+NAME=s_DriveV1#5
+INFO {
+GUID=g_h_4dvxqbg4s95_007n4ybjh1yrv
+CYCLE=i_21
+ACTIVATED=True
+OCCURER=s_proj\Chart.diag
+}
+}
+COMPONENT {
+NAME=s_Agitator#1
+INFO {
+GUID=g_f_000hsn_4t67xld8
+CYCLE=i_21
+ACTIVATED=True
+OCCURER=s_proj\Chart.diag
+}
+}
+COMPONENT {
+NAME=s_PTnode#3
+INFO {
+GUID=g_f_000hsn_51454n9n
+CYCLE=i_21
+ACTIVATED=True
+OCCURER=s_proj\Chart.diag
+}
+}
+COMPONENT {
+NAME=s_PTnode#1
+INFO {
+GUID=g_f_000hsn_51454n9n
+CYCLE=i_21
+ACTIVATED=True
+OCCURER=s_proj\Chart.diag
+}
+}
+COMPONENT {
+NAME=s_Pump#3
+INFO {
+GUID=g_f_000hsn_4ho35doq
+CYCLE=i_21
+ACTIVATED=True
+OCCURER=s_proj\Chart.diag
+}
+}
+COMPONENT {
+NAME=s_Pump#2
+INFO {
+GUID=g_f_000hsn_4ho35doq
+CYCLE=i_21
+ACTIVATED=True
+OCCURER=s_proj\Chart.diag
+}
+}
+COMPONENT {
+NAME=s_Pump#1
+INFO {
+GUID=g_f_000hsn_4ho35doq
+CYCLE=i_21
+ACTIVATED=True
+OCCURER=s_proj\Chart.diag
+}
+}
+COMPONENT {
+NAME=s_DriveV1#4
+INFO {
+GUID=g_h_4dvxqbg4s95_007n4ybjh1yrv
+CYCLE=i_21
+ACTIVATED=True
+OCCURER=s_proj\Chart.diag
+}
+}
+COMPONENT {
+NAME=s_DriveV1#3
+INFO {
+GUID=g_h_4dvxqbg4s95_007n4ybjh1yrv
+CYCLE=i_21
+ACTIVATED=True
+OCCURER=s_proj\Chart.diag
+}
+}
+COMPONENT {
+NAME=s_DriveV1#2
+INFO {
+GUID=g_h_4dvxqbg4s95_007n4ybjh1yrv
+CYCLE=i_21
+ACTIVATED=True
+OCCURER=s_proj\Chart.diag
+}
+}
+COMPONENT {
+NAME=s_DriveV1#1
+INFO {
+GUID=g_h_4dvxqbg4s95_007n4ybjh1yrv
+CYCLE=i_21
+ACTIVATED=True
+OCCURER=s_proj\Chart.diag
+}
+}
+COMPONENT {
+NAME=s_BConnector#4
+INFO {
+GUID=g_h_4dvxpysl1wj_009qz1ckad5ml
+CYCLE=i_21
+ACTIVATED=True
+OCCURER=s_proj\Chart.diag
+}
+}
+COMPONENT {
+NAME=s_BConnector#3
+INFO {
+GUID=g_h_4dvxpysl1wj_009qz1ckad5ml
+CYCLE=i_21
+ACTIVATED=True
+OCCURER=s_proj\Chart.diag
+}
+}
+COMPONENT {
+NAME=s_BConnector#2
+INFO {
+GUID=g_h_4dvxpysl1wj_009qz1ckad5ml
+CYCLE=i_21
+ACTIVATED=True
+OCCURER=s_proj\Chart.diag
+}
+}
+COMPONENT {
+NAME=s_BConnector#1
+INFO {
+GUID=g_h_4dvxpysl1wj_009qz1ckad5ml
+CYCLE=i_21
+ACTIVATED=True
+OCCURER=s_proj\Chart.diag
+}
+}
+COMPONENT {
+NAME=s_Conveyor-S4#1
+INFO {
+GUID=g_f_000hsn_4ho277j6
+CYCLE=i_21
+ACTIVATED=True
+OCCURER=s_proj\Chart.diag
+}
+}
+COMPONENT {
+NAME=s_Digital display#10
+INFO {
+GUID=g_a_0ep6g9_1i6ocrkp
+CYCLE=i_21
+ACTIVATED=True
+OCCURER=s_proj\Chart.diag
+}
+}
+COMPONENT {
+NAME=s_Digital display#9
+INFO {
+GUID=g_a_0ep6g9_1i6ocrkp
+CYCLE=i_21
+ACTIVATED=True
+OCCURER=s_proj\Chart.diag
+}
+}
+COMPONENT {
+NAME=s_FlowIndicator#6
+INFO {
+GUID=g_h_4dvxpyvpfgg_009b8o7fgkbj5
+CYCLE=i_21
+ACTIVATED=True
+OCCURER=s_proj\Chart.diag
+}
+}
+COMPONENT {
+NAME=s_PressureIndicator#2
+INFO {
+GUID=g_f_000hsn_5144qirt
+CYCLE=i_21
+ACTIVATED=True
+OCCURER=s_proj\Chart.diag
+}
+}
+COMPONENT {
+NAME=s_PipeMeasure#16
+INFO {
+GUID=g_f_000hsn_5144ptyw
+CYCLE=i_21
+ACTIVATED=True
+OCCURER=s_proj\Chart.diag
+}
+}
+COMPONENT {
+NAME=s_Valve#9
+INFO {
+GUID=g_f_000hsn_4mh1khmo
+CYCLE=i_21
+ACTIVATED=True
+OCCURER=s_proj\Chart.diag
+}
+}
+COMPONENT {
+NAME=s_PipeMeasure#15
+INFO {
+GUID=g_f_000hsn_5144ptyw
+CYCLE=i_21
+ACTIVATED=True
+OCCURER=s_proj\Chart.diag
+}
+}
+COMPONENT {
+NAME=s_Mnode#2
+INFO {
+GUID=g_f_000hsn_4t3aafzd
+CYCLE=i_21
+ACTIVATED=True
+OCCURER=s_proj\Chart.diag
+}
+}
+COMPONENT {
+NAME=s_Mnode#1
+INFO {
+GUID=g_f_000hsn_4t3aafzd
+CYCLE=i_21
+ACTIVATED=True
+OCCURER=s_proj\Chart.diag
+}
+}
+COMPONENT {
+NAME=s_FlowIndicator#5
+INFO {
+GUID=g_h_4dvxpyvpfgg_009b8o7fgkbj5
+CYCLE=i_21
+ACTIVATED=True
+OCCURER=s_proj\Chart.diag
+}
+}
+COMPONENT {
+NAME=s_PipeMeasure#14
+INFO {
+GUID=g_f_000hsn_5144ptyw
+CYCLE=i_21
+ACTIVATED=True
+OCCURER=s_proj\Chart.diag
+}
+}
+COMPONENT {
+NAME=s_Valve#8
+INFO {
+GUID=g_f_000hsn_4mh1khmo
+CYCLE=i_21
+ACTIVATED=True
+OCCURER=s_proj\Chart.diag
+}
+}
+COMPONENT {
+NAME=s_PressureIndicator#7
+INFO {
+GUID=g_f_000hsn_5144qirt
+CYCLE=i_21
+ACTIVATED=True
+OCCURER=s_proj\Chart.diag
+}
+}
+COMPONENT {
+NAME=s_PipeMeasure#13
+INFO {
+GUID=g_f_000hsn_5144ptyw
+CYCLE=i_21
+ACTIVATED=True
+OCCURER=s_proj\Chart.diag
+}
+}
+COMPONENT {
+NAME=s_Digital display#8
+INFO {
+GUID=g_a_0ep6g9_1i6ocrkp
+CYCLE=i_21
+ACTIVATED=True
+OCCURER=s_proj\Chart.diag
+}
+}
+COMPONENT {
+NAME=s_Digital display#7
+INFO {
+GUID=g_a_0ep6g9_1i6ocrkp
+CYCLE=i_21
+ACTIVATED=True
+OCCURER=s_proj\Chart.diag
+}
+}
+COMPONENT {
+NAME=s_LevelIndicator#1
+INFO {
+GUID=g_f_000hsn_5144ouy6
+CYCLE=i_21
+ACTIVATED=True
+OCCURER=s_proj\Chart.diag
+}
+}
+COMPONENT {
+NAME=s_FlowIndicator#4
+INFO {
+GUID=g_h_4dvxpyvpfgg_009b8o7fgkbj5
+CYCLE=i_21
+ACTIVATED=True
+OCCURER=s_proj\Chart.diag
+}
+}
+COMPONENT {
+NAME=s_PipeMeasure#12
+INFO {
+GUID=g_f_000hsn_5144ptyw
+CYCLE=i_21
+ACTIVATED=True
+OCCURER=s_proj\Chart.diag
+}
+}
+COMPONENT {
+NAME=s_Valve#6
+INFO {
+GUID=g_f_000hsn_4mh1khmo
+CYCLE=i_21
+ACTIVATED=True
+OCCURER=s_proj\Chart.diag
+}
+}
+COMPONENT {
+NAME=s_PressureIndicator#6
+INFO {
+GUID=g_f_000hsn_5144qirt
+CYCLE=i_21
+ACTIVATED=True
+OCCURER=s_proj\Chart.diag
+}
+}
+COMPONENT {
+NAME=s_PipeMeasure#11
+INFO {
+GUID=g_f_000hsn_5144ptyw
+CYCLE=i_21
+ACTIVATED=True
+OCCURER=s_proj\Chart.diag
+}
+}
+COMPONENT {
+NAME=s_PTnode#6
+INFO {
+GUID=g_f_000hsn_51454n9n
+CYCLE=i_21
+ACTIVATED=True
+OCCURER=s_proj\Chart.diag
+}
+}
+COMPONENT {
+NAME=s_StorageTankLiquid#1
+INFO {
+GUID=g_h_4dvxpyts1t6_009siwvz0jm06
+CYCLE=i_21
+ACTIVATED=True
+OCCURER=s_proj\Chart.diag
+}
+}
+COMPONENT {
+NAME=s_Digital display#6
+INFO {
+GUID=g_a_0ep6g9_1i6ocrkp
+CYCLE=i_21
+ACTIVATED=True
+OCCURER=s_proj\Chart.diag
+}
+}
+COMPONENT {
+NAME=s_Digital display#5
+INFO {
+GUID=g_a_0ep6g9_1i6ocrkp
+CYCLE=i_21
+ACTIVATED=True
+OCCURER=s_proj\Chart.diag
+}
+}
+COMPONENT {
+NAME=s_Digital display#4
+INFO {
+GUID=g_a_0ep6g9_1i6ocrkp
+CYCLE=i_21
+ACTIVATED=True
+OCCURER=s_proj\Chart.diag
+}
+}
+COMPONENT {
+NAME=s_FlowIndicator#3
+INFO {
+GUID=g_h_4dvxpyvpfgg_009b8o7fgkbj5
+CYCLE=i_21
+ACTIVATED=True
+OCCURER=s_proj\Chart.diag
+}
+}
+COMPONENT {
+NAME=s_PipeMeasure#10
+INFO {
+GUID=g_f_000hsn_5144ptyw
+CYCLE=i_21
+ACTIVATED=True
+OCCURER=s_proj\Chart.diag
+}
+}
+COMPONENT {
+NAME=s_Valve#5
+INFO {
+GUID=g_f_000hsn_4mh1khmo
+CYCLE=i_21
+ACTIVATED=True
+OCCURER=s_proj\Chart.diag
+}
+}
+COMPONENT {
+NAME=s_FlowIndicator#2
+INFO {
+GUID=g_h_4dvxpyvpfgg_009b8o7fgkbj5
+CYCLE=i_21
+ACTIVATED=True
+OCCURER=s_proj\Chart.diag
+}
+}
+COMPONENT {
+NAME=s_PipeMeasure#8
+INFO {
+GUID=g_f_000hsn_5144ptyw
+CYCLE=i_21
+ACTIVATED=True
+OCCURER=s_proj\Chart.diag
+}
+}
+COMPONENT {
+NAME=s_Valve#4
+INFO {
+GUID=g_f_000hsn_4mh1khmo
+CYCLE=i_21
+ACTIVATED=True
+OCCURER=s_proj\Chart.diag
+}
+}
+COMPONENT {
+NAME=s_Box#1
+INFO {
+GUID=g_f_000hsn_4ho2b2q1
+CYCLE=i_0
+ACTIVATED=True
+OCCURER=s_storage\List.diag
+}
+}
+COMPONENT {
+NAME=s_FlowIndicator#1
+INFO {
+GUID=g_h_4dvxpyvpfgg_009b8o7fgkbj5
+CYCLE=i_21
+ACTIVATED=True
+OCCURER=s_proj\Chart.diag
+}
+}
+COMPONENT {
+NAME=s_PipeMeasure#2
+INFO {
+GUID=g_f_000hsn_5144ptyw
+CYCLE=i_21
+ACTIVATED=True
+OCCURER=s_proj\Chart.diag
+}
+}
+COMPONENT {
+NAME=s_Valve#1
+INFO {
+GUID=g_f_000hsn_4mh1khmo
+CYCLE=i_21
+ACTIVATED=True
+OCCURER=s_proj\Chart.diag
+}
+}
